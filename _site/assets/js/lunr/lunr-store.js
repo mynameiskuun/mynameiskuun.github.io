cf15e56bade6175ce10e98c5382c7eeb6ac16292
mynameiskuun <mynameiskuun@gmail.com>
2023-09-05 23:54:39 +0900

    blog SQL solution update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Post: SQL 문제 풀이 - 프로그래머스",
-        "excerpt":"서울에 위치한 식당 목록 출력하기   문제 바로가기  ","categories": ["Problem Solving"],
+        "excerpt":"프로그래머스 SQL 코딩테스트 연습 서울에 위치한 식당 목록 출력하기 REST_INFO와 REST_REVIEW 테이블에서 서울에 위치한 식당들의 식당 ID, 식당 이름, 음식 종류, 즐겨찾기수, 주소, 리뷰 평균 점수를 조회하는 SQL문을 작성해주세요. 이때 리뷰 평균점수는 소수점 세 번째 자리에서 반올림 해주시고 결과는 평균점수를 기준으로 내림차순 정렬해주시고, 평균점수가 같다면 즐겨찾기수를 기준으로 내림차순 정렬해주세요. 문제...","categories": ["Problem Solving"],
         "tags": ["SQL"],
         "url": "/problem%20solving/SQL-%EB%AC%B8%EC%A0%9C%ED%92%80%EC%9D%B4(1)/",
         "teaser": null

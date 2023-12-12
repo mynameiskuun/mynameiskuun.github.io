f67fa9afb62c468bdb873cb19d4ca11d2888af5d
mynameiskuun <mynameiskuun@gmail.com>
2023-12-13 02:29:37 +0900

    Study : "Algorithm - 가장 짧은 문자거리 찾기"

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
-        "title": "Post: SQL 문제 풀이 - 프로그래머스",
+        "title": "Post: SQL 문제 풀이 - SELECT",
         "excerpt":"프로그래머스 SQL 코딩테스트 연습 서울에 위치한 식당 목록 출력하기 REST_INFO와 REST_REVIEW 테이블에서 서울에 위치한 식당들의 식당 ID, 식당 이름, 음식 종류, 즐겨찾기수, 주소, 리뷰 평균 점수를 조회하는 SQL문을 작성해주세요. 이때 리뷰 평균점수는 소수점 세 번째 자리에서 반올림 해주시고 결과는 평균점수를 기준으로 내림차순 정렬해주시고, 평균점수가 같다면 즐겨찾기수를 기준으로 내림차순 정렬해주세요. 문제...","categories": ["Problem Solving"],
         "tags": ["SQL"],
         "url": "/problem%20solving/SQL-%EB%AC%B8%EC%A0%9C%ED%92%80%EC%9D%B4(1)/",
+        "teaser": null
+      },{
+        "title": "Post: SQL 문제 풀이 - SELECT",
+        "excerpt":"프로그래머스 SQL 코딩테스트 연습 오프라인/온라인 판매 데이터 통합하기 ONLINE_SALE 테이블과 OFFLINE_SALE 테이블에서 2022년 3월의 오프라인/온라인 상품 판매 데이터의 판매 날짜, 상품ID, 유저ID, 판매량을 출력하는 SQL문을 작성해주세요. OFFLINE_SALE 테이블의 판매 데이터의 USER_ID 값은 NULL 로 표시해주세요. 결과는 판매일을 기준으로 오름차순 정렬해주시고 판매일이 같다면 상품 ID를 기준으로 오름차순, 상품ID까지 같다면 유저 ID를...","categories": ["Problem Solving"],
+        "tags": ["SQL"],
+        "url": "/problem%20solving/SQL-%EB%AC%B8%EC%A0%9C%ED%92%80%EC%9D%B4(2)/",
         "teaser": null
       }]
